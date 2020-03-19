15a0c6a18b4ed06be2d8bba17757180ee844e511
Abdulrahman Y. idlbi <adlogi@gmail.com>
2020-03-20 00:18:00 +0300

    creating the portfolio page

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,20 +1,69 @@
 import React, { Component } from 'react';
 import SideMenu from './SideMenu';
+import moviexplorer from '../media/moviexplorer.png';
+import minesweeper from '../media/minesweeper.png';
+import abbuloka from '../media/abbuloka.png';
+import Carousel from 'react-bootstrap/Carousel';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLink } from '@fortawesome/free-solid-svg-icons';
 
 export default class Portfolio extends Component {
   render() {
     return (
       <div className="container-fluid fade-in">
         <div className="row h-100">
-          <SideMenu selected='portfolio' />
-          <div className="col-xl-9 col-lg-12 h-100">
+          <div className="col-md-4 col-lg-3 align-self-center">
+            <SideMenu selected='portfolio' />
+          </div>
+          <div className="col-md-8 col-lg-9 flex-grow-1 my-5">
             <div className="row align-items-center h-100">
-              <div className="col-12 section-link pr-5 pl-4 pl-xl-0">
-                <h2>Selam!</h2>
-                <p>I'm Abdulrahman. I was born in Saudi Arabia, grew up in Syria, studied in the US, and now living in Turkey. I'm a front-end developer with experince in ReactJS.</p>
-                <p>I studied computer engineering and was part of the team at MIT that develops Scratch: the children's most popular programming language. In a parallel life, I design learning spaces and activities for children.</p>
+              <div className="col px-5">
+                <Carousel fade={true}>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100"
+                      src={moviexplorer}
+                      alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>movieXplorer</h3>
+                        <p>
+                          <a href="https://github.com/adlogi/movieXplorer" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /></a>{' '}
+                          <a href="#" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLink} /></a>
+                        </p>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100"
+                      src={minesweeper}
+                      alt="Second slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>Minesweeper</h3>
+                        <p>
+                          <a href="https://github.com/adlogi/minesweeper" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /></a>{' '}
+                          <a href="https://adlogi.github.io/minesweeper/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLink} /></a>
+                        </p>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100"
+                      src={abbuloka}
+                      alt="Third slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>Abbuloka</h3>
+                        <p>
+                          <a href="https://github.com/bsurmen/abbulokapremium" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /></a>{' '}
+                          <a href="https://abbuloka.netlify.com/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLink} /></a>
+                        </p>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                </Carousel>
               </div>
-              
             </div>
           </div>
         </div>
