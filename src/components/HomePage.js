981cfb7ff3868fe652bd2f2796e20434289329f8
Abdulrahman Y. idlbi <adlogi@gmail.com>
2020-03-19 17:15:54 +0300

    add links to other pages

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import '../style/HomePage.css'
+import '../style/HomePage.css';
+import { Link } from 'react-router-dom';
+
 
 export default class HomePage extends Component {
   render() {
@@ -8,16 +10,24 @@ export default class HomePage extends Component {
         <div className="row h-100">
           <div className="col-xl-3 col-lg-12 align-self-center">
             <h1 className="short-name text-xl-center text-lg-left">
-              A<span className="colored">.</span>Y<span className="colored">.</span>idlbi<span className="smile colored">)</span>
+            <Link to='./'>A<span className="colored">.</span>Y<span className="colored">.</span>idlbi<span className="smile colored">)</span></Link>
             </h1>
           </div>
           <div className="col-xl-9 col-lg-12 h-100">
             <div className="row align-items-center h-100">
               <div className="col-12 side-bar h-100"></div>
-              <div className="col-12 section-link text-center h-25"><span>BIO</span></div>
-              <div className="col-12 section-link text-center h-25"><span>RESUME</span></div>
-              <div className="col-12 section-link text-center h-25"><span>PORTFOLIO</span></div>
-              <div className="col-12 section-link text-center h-25"><span>CONTACT</span></div>
+              <div className="col-12 section-link text-center h-25">
+                <span><Link to='./bio'>BIO</Link></span>
+              </div>
+              <div className="col-12 section-link text-center h-25">
+                <span><Link to='./resume'>RESUME</Link></span>
+              </div>
+              <div className="col-12 section-link text-center h-25">
+                <span><Link to='./portfolio'>PORTFOLIO</Link></span>
+              </div>
+              <div className="col-12 section-link text-center h-25">
+                <span><Link to='./contact'>CONTACT</Link></span>
+              </div>
             </div>
           </div>
         </div>
