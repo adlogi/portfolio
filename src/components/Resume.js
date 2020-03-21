7be99f5095214b234972c24aa12c28ccc70352da
Abdulrahman Y. idlbi <adlogi@gmail.com>
2020-03-21 17:44:33 +0300

    use bootstrap accordion element instead of nav tabs to show resume sections

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,8 +1,36 @@
 import React, { Component } from 'react';
 import SideMenu from './SideMenu';
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
 import '../style/Resume.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronCircleRight as chevronIcon} from '@fortawesome/free-solid-svg-icons';
+// import { faAngleDoubleRight as chevronIcon} from '@fortawesome/free-solid-svg-icons';
 
 export default class Resume extends Component {
+  // remove the 'show-header' class from all card headers
+  clearHeaders = () => {
+    const cardHeaders = document.querySelectorAll('.card-header');
+    cardHeaders.forEach(header => {
+      header.classList.remove('show-header');
+    });
+  }
+
+  // toggle a card header on clicking by toggling the 'show-header' class
+  handleToggle = (event) => {
+    let cardHeader = event.nativeEvent.target;
+    if (cardHeader.nodeName === 'SPAN') {
+      cardHeader = cardHeader.parentNode;
+    }
+    const isShown = cardHeader.classList.contains('show-header');
+    this.clearHeaders();
+    if (isShown) {
+      cardHeader.classList.remove('show-header');
+    } else {
+      cardHeader.classList.add('show-header');
+    }
+  }
+
   render() {
     return (
       <div className="container-fluid fade-in">
@@ -10,37 +38,69 @@ export default class Resume extends Component {
           <SideMenu selected='resume' />
           <div className="offset-md-4 col-md-8 offset-lg-3 col-lg-9 flex-grow-1 my-5">
             <div className="row align-items-center h-100">
-              <div className="col px-5 h-75">
-                <nav>
-                  <ul className="nav nav-tabs nav-fill nav-primary" id="nav-tabs" role="tablist">
-                    <li className="nav-item nav-link active" id="nav-summary-tab" data-toggle="tab" role="tab" aria-controls="nav-summary" aria-selected="true">Summary</li>
-                    <li className="nav-item nav-link" id="nav-experience-tab" data-toggle="tab" role="tab" aria-controls="nav-experience" aria-selected="false">Technical Experience</li>
-                    <li className="nav-item nav-link" id="nav-skills-tab" data-toggle="tab" role="tab" aria-controls="nav-skills" aria-selected="false">Technical Skills</li>
-                    <li className="nav-item nav-link" id="nav-work-tab" data-toggle="tab" role="tab" aria-controls="nav-work" aria-selected="false">Work Experience</li>
-                    <li className="nav-item nav-link" id="nav-education-tab" data-toggle="tab" role="tab" aria-controls="nav-education" aria-selected="false">Education</li>
-                  </ul>
-                </nav>
-                <div className="tab-content h-75" id="nav-tabContent">
-                  <div className="tab-pane fade show active" id="nav-summary" role="tabpanel" aria-labelledby="nav-summary-tab">
-                    <p className="mt-3 mb-0">Front-end web developer with a passion for [their personal mission].  With experience in Ruby on Rails, JavaScript, and React and a background in X, I discovered web development through  [Y]. I bring strong skills in [team-building and project management] that help [what type of companies] [drive what positive result/impact]. [Other unique anecdote]. With a background in computer engineering and learning sciences, I know how to approach people and technology.</p>
-                  </div>
-                  <div className="tab-pane fade" id="nav-experience" role="tabpanel" aria-labelledby="nav-experience-tab">
-                    Technical Experience
-                  </div>
-                  <div className="tab-pane fade" id="nav-skills" role="tabpanel" aria-labelledby="nav-skills-tab">
-                    Technical Skills
-                  </div>
-                  <div className="tab-pane fade" id="nav-work" role="tabpanel" aria-labelledby="nav-work-tab">
-                    Work Experience
-                  </div>
-                  <div className="tab-pane fade" id="nav-education" role="tabpanel" aria-labelledby="nav-education-tab">
-                    <p className="mt-3 mb-0">Front-End Web Development Program, Flatiron School (2020)</p>
-                    <p className="text-muted smaller">JavaScript & React online program / Re:Coded Istanbul Bootcamp.</p>
-                    <p className="mt-3 mb-0">SM in Media Arts & Sciences, MIT (2014)</p>
-                    <small className="text-muted smaller">The Lifelong Kindergarten research group, developing new technologies and activities that engage people in creative learning experiences.</small>
-                    <p className="mt-3 mb-0">BS in Computer Engineering, Damascus University (2009)</p>
-                  </div>
-                </div>
+              <div className="col px-5">
+
+                <Accordion defaultActiveKey="0">
+                  <Card>
+                    <Accordion.Toggle as={Card.Header} eventKey="0" onClick={this.handleToggle}>
+                      <span>Summary</span>
+                      <span><FontAwesomeIcon icon={chevronIcon} /></span>
+                    </Accordion.Toggle>
+                    <Accordion.Collapse eventKey="0">
+                      <Card.Body>
+                        <p className="mb-0">Front-end web developer with a passion for [their personal mission].  With experience in Ruby on Rails, JavaScript, and React and a background in X, I discovered web development through  [Y]. I bring strong skills in [team-building and project management] that help [what type of companies] [drive what positive result/impact]. [Other unique anecdote]. With a background in computer engineering and learning sciences, I know how to approach people and technology.</p>
+                      </Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                  <Card>
+                    <Accordion.Toggle as={Card.Header} eventKey="1" onClick={this.handleToggle}>
+                      <span>Technical Experience</span>
+                      <span><FontAwesomeIcon icon={chevronIcon} /></span>
+                    </Accordion.Toggle>
+                    <Accordion.Collapse eventKey="1">
+                      <Card.Body>Hello! I'm another body</Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                  <Card>
+                    <Accordion.Toggle as={Card.Header} eventKey="2" onClick={this.handleToggle}>
+                      <span>Technical Skills</span>
+                      <span><FontAwesomeIcon icon={chevronIcon} /></span>
+                    </Accordion.Toggle>
+                    <Accordion.Collapse eventKey="2">
+                      <Card.Body>Hello! I'm another body</Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                  <Card>
+                    <Accordion.Toggle as={Card.Header} eventKey="3" onClick={this.handleToggle}>
+                      <span>Work Experience</span>
+                      <span><FontAwesomeIcon icon={chevronIcon} /></span>
+                    </Accordion.Toggle>
+                    <Accordion.Collapse eventKey="3">
+                      <Card.Body>
+                        <p className="mb-0">Creative Learning Consultant (2014 - present)</p>
+                        <p className="text-muted smaller">JavaScript & React online program / Re:Coded Istanbul Bootcamp.</p>
+                        <p className="mt-3 mb-0">SM in Media Arts & Sciences, MIT (2014)</p>
+                        <small className="text-muted smaller">The Lifelong Kindergarten research group, developing new technologies and activities that engage people in creative learning experiences.</small>
+                        <p className="mt-3 mb-0">BS in Computer Engineering, Damascus University (2009)</p>
+                      </Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                  <Card>
+                    <Accordion.Toggle as={Card.Header} eventKey="4" onClick={this.handleToggle}>
+                      <span>Education</span>
+                      <span><FontAwesomeIcon icon={chevronIcon} /></span>
+                    </Accordion.Toggle>
+                    <Accordion.Collapse eventKey="4">
+                      <Card.Body>
+                        <p className="mb-0">Front-End Web Development Program, Flatiron School (2020)</p>
+                        <p className="text-muted smaller">JavaScript & React online program / Re:Coded Istanbul Bootcamp.</p>
+                        <p className="mt-3 mb-0">SM in Media Arts & Sciences, MIT (2014)</p>
+                        <small className="text-muted smaller">The Lifelong Kindergarten research group, developing new technologies and activities that engage people in creative learning experiences.</small>
+                        <p className="mt-3 mb-0">BS in Computer Engineering, Damascus University (2009)</p>
+                      </Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                </Accordion>
               </div>
             </div>
           </div>
@@ -50,22 +110,11 @@ export default class Resume extends Component {
   }
 
   componentDidMount() {
-    const navTabs = document.querySelectorAll('#nav-tabs > .nav-item');
-    const tabPanes = document.querySelectorAll('#nav-tabContent > .tab-pane');
-    navTabs.forEach(tab => {
-      tab.addEventListener('click', (event) => {
-        for (let i = 0; i < navTabs.length; i++) {
-          navTabs[i].classList.remove('active');
-          navTabs[i].setAttribute("aria-selected", false);
-          tabPanes[i].classList.remove('active', 'show');
-        }
-        
-        const currentTab = event.target;
-        currentTab.classList.add('active');
-        currentTab.setAttribute("aria-selected", true);
-        const currentPane = document.querySelector(`#${currentTab.id.slice(0, -4)}`);
-        currentPane.classList.add('active', 'show');
-      })
+    const cardHeaders = document.querySelectorAll('.card-header');
+    cardHeaders.forEach((header) => {
+      header.classList.add('d-flex', 'justify-content-between');
     });
+    // the first card is shown by default
+    cardHeaders[0].classList.add('show-header');
   }
 }
