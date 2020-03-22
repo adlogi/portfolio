3824b9db45a6df20688438f2fbfe619494963fe7
Abdulrahman Y. idlbi <adlogi@gmail.com>
2020-03-22 14:34:38 +0300

    Updates

---
+++ precache-manifest.4dbdbd924fcca2ee736ad1c177c3dfa3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b206626b6150cccf98f1567d056b4889",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05c1d03a0c1c05ca1969",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "f373e3905957f06b1f97",
+    "url": "/static/css/main.296392b4.chunk.css"
+  },
+  {
+    "revision": "05c1d03a0c1c05ca1969",
+    "url": "/static/js/2.197fdad5.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.197fdad5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f373e3905957f06b1f97",
+    "url": "/static/js/main.83c1ec76.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "dc60bc4055483dd131f3fca096f03339",
+    "url": "/static/media/abbuloka.dc60bc40.png"
+  },
+  {
+    "revision": "1caa8e0556a39a0627593a0c9c8aca8c",
+    "url": "/static/media/bio.1caa8e05.jpg"
+  },
+  {
+    "revision": "a6389a5e392297bcb1de00b2731ca06a",
+    "url": "/static/media/contact.a6389a5e.jpg"
+  },
+  {
+    "revision": "e7cd9f6cadf3a1b48915ecbdffb4886e",
+    "url": "/static/media/minesweeper.e7cd9f6c.png"
+  },
+  {
+    "revision": "f7845d6884bb2306a662ba9fd722ca1e",
+    "url": "/static/media/moviexplorer.f7845d68.png"
+  },
+  {
+    "revision": "72c0599932c30a42eb7f4217a0a0e00c",
+    "url": "/static/media/portfolio.72c05999.jpg"
+  },
+  {
+    "revision": "851decbaef1f0433e236856e4ba79e11",
+    "url": "/static/media/resume.851decba.jpg"
+  }
+]);
