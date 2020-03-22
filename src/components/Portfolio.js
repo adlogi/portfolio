c24d5326ec61c03fe987683eeec46041ff29e00a
Abdulrahman Y. idlbi <adlogi@gmail.com>
2020-03-23 02:34:51 +0300

    moving portfolio data to a separate json file

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -10,6 +10,15 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faLink } from '@fortawesome/free-solid-svg-icons';
 
 export default class Portfolio extends Component {
+  constructor(props) {
+    super(props);
+    this.portfolio = require('../data/portfolio.json');
+    this.images = {
+      'movieXplorer': moviexplorer,
+      'Minesweeper': minesweeper,
+      'Abbuloka': abbuloka,
+    }
+  }
   render() {
     return (
       <div className="container-fluid fade-in">
@@ -19,48 +28,22 @@ export default class Portfolio extends Component {
             <div className="row align-items-center h-100">
               <div className="col px-5">
                 <Carousel fade={true}>
-                  <Carousel.Item>
-                    <img
-                      className="d-block w-100"
-                      src={moviexplorer}
-                      alt="First slide"
-                    />
-                    <Carousel.Caption>
-                      <h3>movieXplorer</h3>
-                      <p className="icons">
-                        <a href="https://adlogi.github.io/movieXplorer/" rel="noopener noreferrer"><FontAwesomeIcon icon={faLink} /></a>{' '}
-                        <a href="https://github.com/adlogi/movieXplorer" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /></a>
-                      </p>
-                    </Carousel.Caption>
-                  </Carousel.Item>
-                  <Carousel.Item>
-                    <img
-                      className="d-block w-100"
-                      src={minesweeper}
-                      alt="Second slide"
-                    />
-                    <Carousel.Caption>
-                      <h3>Minesweeper</h3>
-                      <p className="icons">
-                        <a href="https://adlogi.github.io/minesweeper/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLink} /></a>{' '}
-                        <a href="https://github.com/adlogi/minesweeper" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /></a>
-                      </p>
-                    </Carousel.Caption>
-                  </Carousel.Item>
-                  <Carousel.Item>
-                    <img
-                      className="d-block w-100"
-                      src={abbuloka}
-                      alt="Third slide"
-                    />
-                    <Carousel.Caption>
-                      <h3>Abbuloka</h3>
-                      <p className="icons">
-                        <a href="https://abbuloka.netlify.com/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLink} /></a>{' '}
-                        <a href="https://github.com/bsurmen/abbulokapremium" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /></a>
-                      </p>
-                    </Carousel.Caption>
-                  </Carousel.Item>
+                  {this.portfolio.map((project, index) => (
+                    <Carousel.Item key={index}>
+                      <img
+                        className="d-block w-100"
+                        src={this.images[project.name]}
+                        alt={project.name}
+                      />
+                      <Carousel.Caption>
+                        <h3>{project.name}</h3>
+                        <p className="icons">
+                          <a href={project.live} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLink} /></a>{' '}
+                          <a href={project.github} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /></a>
+                        </p>
+                      </Carousel.Caption>
+                    </Carousel.Item>
+                  ))}
                 </Carousel>
               </div>
             </div>
