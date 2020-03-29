c44f4a897511a61cee537a5c17985a9e8faa0841
Abdulrahman Y. idlbi <adlogi@gmail.com>
2020-03-29 18:44:42 +0300

    minor edits

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -11,7 +11,7 @@ export default class Bio extends Component {
             <div className="row align-items-center h-100">
               <div className="col px-5">
                 <h2>Selam!</h2>
-                <p>I'm Abdulrahman — a.k.a. <span className="font-italic">@adlogi</span>. I was born in Saudi Arabia, grew up in Syria, studied in the US, and now living in Turkey. I'm a front-end developer with experince in ReactJS.</p>
+                <p>I'm Abdulrahman Y. idlbi — a.k.a. <span className="font-italic">@adlogi</span>. I was born in Saudi Arabia, grew up in Syria, studied in the US, and now living in Turkey. I'm a front-end developer with experince in ReactJS.</p>
                 <p>I see coding as a form of expression: a paintbrush. And I like to use it to create experiences that help others express themselves too.</p>
                 <p>I studied computer engineering and learning sciences. I was part of the team at MIT that develops <a href="https://scratch.mit.edu/" target="_blank" rel="noopener noreferrer" className="font-italic">Scratch</a>: the children's most popular programming language. Beside coding, I design creative learning spaces and activities for children in various contexts. Did you watch <a href="https://www.youtube.com/watch?v=uOwWqOHE6ng" target="_blank" rel="noopener noreferrer" className="font-italic">my talk</a> about keeping children motivated to learn?</p>
               </div>
